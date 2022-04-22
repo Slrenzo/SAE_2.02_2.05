ceb66005fe9ea1a5047475fb46e8c1c80f7b00d0
Enzo <enzo.soulier@iut-rodez.fr>
2022-04-22 08:57:49 +0200

    Création des classes

---
+++ src/sae/pert/Projet.java
@@ -0,0 +1,17 @@
+/*
+ * Projet.java                                  22 avr. 2022
+ * IUT de Rodez, Info1 2021-2022, pas de copyright 
+ */
+package sae.pert;
+
+/** 
+ * Un projet est un ensemble de taches que l'on doit ordonné pour arriver à
+ * un objectif.
+ * @author Thomas Nalix
+ * @author Lorentin Nicolas
+ * @author Emilien Restoueix
+ * @author Enzo Soulier
+ */
+public class Projet {
+
+}
--- src/sae/pert/Tache.java
+++ src/sae/pert/Tache.java
@@ -1,14 +1,34 @@
 /*
- * TODO date
+ * Tache.java                                  22 avr. 2022
+ * IUT de Rodez, Info1 2021-2022, pas de copyright 
  */
 
 package sae.pert;
 
 /**
- * TODO faire javadoc
- * @author Soulier
- *
+ * Tache permettant de faire avancer un projet.
+ * @author Thomas Nalix
+ * @author Lorentin Nicolas
+ * @author Emilien Restoueix
+ * @author Enzo Soulier
  */
 public class Tache {
-
+    
+    /* Nom attribué à la tache */
+    private String nom;
+    
+    /* Description synthétique de la tache */
+    private String description;
+    
+    /* 
+     * Durée que la tache prendra a être réalisée
+     * (L'unité de temps sera défini dans le projet ou est utilisé la tache)
+     */
+    private double duree;
+    
+    public Tache(String nom, String description, double duree) {
+        if (nom.isBlank() || nom == null) {
+            throw new IllegalArgumentException("Le nom est vide ou null");
+        }
+    }
 }
