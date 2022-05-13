dc496b809f39f1581b402b01f54c59fd320af2e3
emilien <emilien.restoueix@iut-rodez.fr>
2022-05-13 13:59:40 +0200

    test Tache

--- src/sae/pert/tests/testTache.java
+++ src/sae/pert/tests/testTache.java
@@ -19,17 +19,17 @@ import testOutillage.assertionTest;
 public class testTache {
     
     /**
-     * Jeux de tests unitaires qui servira a tester des méthodes
+     * Jeux de tests unitaires qui servira a tester des mï¿½thodes
      */
     private static Tache[] aTester = {
-        new Tache("Tache A", "Cette tache consiste à analyser le besoin", 2.0),
-        new Tache("Tache B", "Mise en place des méthodes de travail", 5.0),
-        new Tache("Tache C", "Répartition du travail", 3.5),
-        new Tache("Tache D", "Réalisation de l'application", 30.0)
+        new Tache("Tache A", "Cette tache consiste Ã  analyser le besoin", 2.0, 2.0, 8.0),
+        new Tache("Tache B", "Mise en place des mÃ©thodes de travail", 5.0, 7.0, 12.0),
+        new Tache("Tache C", "RÃ©partition du travail", 3.5, 7.0, 12.5),
+        new Tache("Tache D", "RÃ©alisation de l'application", 30.0, 12.0, 42.0)
     };
     
     /**
-     * Lance les différents jeux de tests
+     * Lance les diffï¿½rents jeux de tests
      * @param args
      */
     public static void main(String[] args) {
@@ -41,16 +41,21 @@ public class testTache {
         ok &= testGetDescription();
         ok &= testGetDuree();
         ok &= testToString();
-        ok &= testSetNom();
         ok &= testSetDescription();
         ok &= testSetDuree();
         ok &= testAjouterTachePrealable();
         ok &= testEnleverTachePrealable();
+        ok &= testSetDateAuPlusTot();
+        ok &= testGetDateAuPlusTot();
+        ok &= testGetDateAuPlusTard();
+        ok &= testSetDateAuPlusTot();
+        ok &= testSetDateAuPlusTard();
+        
         
         if(ok) {
-            System.out.println("Test réussis");
+            System.out.println("Test rÃ©ussis");
         } else {
-            System.out.println("Test échoué");
+            System.out.println("Test Ã©chouÃ©");
         }
     }
 
@@ -63,49 +68,77 @@ public class testTache {
         boolean testOK;
         
         try {
-            new Tache(null, "Répartition du travail", 30.5);
+            new Tache(null, "RÃ©partition du travail", 30.5, 5.0, 35.5);
             testOK = false;
-        } catch (NullPointerException nomNul) {
+        } catch (NullPointerException nomNull) {
             testOK = true;
         }
         
         try {
-            new Tache("  ", "Répartition du travail", 30.5);
+            new Tache("  ", "RÃ©partition du travail", 30.5, 5.0, 35.5);
             testOK = false;
         } catch (IllegalArgumentException nomVide) {
             testOK = true;
         }
         
         try {
-            new Tache("Tache A", null, 30.5);
+            new Tache("Tache A", null, 30.5, 5.0, 35.5);
             testOK = false;
-        } catch (NullPointerException descriptionNul) {
+        } catch (NullPointerException descriptionNull) {
             testOK = true;
         }
         
         try {
-            new Tache("Tache A", "", 30.5);
+            new Tache("Tache A", "", 30.5, 5.0, 35.5);
             testOK = false;
         } catch (IllegalArgumentException descriptionVide) {
             testOK = true;
         }
         
         try {
-            new Tache("Tache A", "Répartition du travail", 0.0);
+            new Tache("Tache A", "RÃ©partition du travail", 0.0, 5.0, 35.5);
             testOK = false;
-        } catch (IllegalArgumentException dureeNul) {
+        } catch (IllegalArgumentException dureeNull) {
             testOK = true;
         }
         
         try {
-            new Tache("Tache A", "Répartition du travail", -5.0);
+            new Tache("Tache A", "RÃ©partition du travail", -5.0, 5.0, 35.5);
             testOK = false;
-        } catch (IllegalArgumentException dureeNégative) {
+        } catch (IllegalArgumentException dureeNegative) {
             testOK = true;
         }
         
         try {
-            new Tache("Tache A", "Répartition du travail", 35.5);
+            new Tache("Tache A", "RÃ©partition du travail", 30.5, 0.0, 35.5);
+            testOK = false;
+        } catch (IllegalArgumentException dateAuPlusTotNull) {
+            testOK = true;
+        }
+        
+        try {
+            new Tache("Tache A", "RÃ©partition du travail", 30.5, -5.0, 35.5);
+            testOK = false;
+        } catch (IllegalArgumentException dateAuPlusTotNegative) {
+            testOK = true;
+        }
+        
+        try {
+            new Tache("Tache A", "RÃ©partition du travail", 30.5, 5.0, 0.0);
+            testOK = false;
+        } catch (IllegalArgumentException dateAuPlusTardNull) {
+            testOK = true;
+        }
+        
+        try {
+            new Tache("Tache A", "RÃ©partition du travail", 30.5, 5.0, -35.5);
+            testOK = false;
+        } catch (IllegalArgumentException dateAuPlusTardNegative) {
+            testOK = true;
+        }
+        
+        try {
+            new Tache("Tache A", "RÃ©partition du travail", 30.5, 5.0, 35.5);
             testOK = true;
         } catch (IllegalArgumentException TacheIncorrect) {
             testOK = false;
@@ -138,10 +171,10 @@ public class testTache {
     private static boolean testGetDescription() {
         
         String[] descriptionAttendus = {
-            "Cette tache consiste à analyser le besoin",
-            "Mise en place des méthodes de travail",
-            "Répartition du travail",
-            "Réalisation de l'application"
+            "Cette tache consiste ï¿½ analyser le besoin",
+            "Mise en place des mï¿½thodes de travail",
+            "Rï¿½partition du travail",
+            "Rï¿½alisation de l'application"
         };
         
         boolean ok;
@@ -172,31 +205,6 @@ public class testTache {
         return ok;
     }
     
-    /** Test unitaires de setNom
-     * @return true si test reussis sinon false 
-     */
-    private static boolean testSetNom() {
-        
-        boolean ok;
-        
-        /** Tache de test */
-        Tache test = new Tache("Tache A", "Répartition du travail", 30.5);
-        String[] testNom = {"Tache 1", "Tache_A", "Tache-1", "    "};
-        
-        String[] nomAttendus = {"Tache 1", "Tache_A", "Tache-1", "Tache A"};
-        
-        ok = true;
-        for (int noTest = 0; ok && noTest < testNom.length; noTest++) {
-            try {
-                test.setNom(testNom[noTest]);
-                ok &= test.getNom().equals(nomAttendus[noTest]);
-            } catch (IllegalArgumentException nomIncorrect) {
-                ok = true;
-            }
-        }
-        return ok;
-    }
-    
     /** Test unitaires de setDescription
      * @return true si test reussis sinon false 
      */
@@ -205,10 +213,10 @@ public class testTache {
         boolean ok;
         
         /** Tache de test */
-        Tache test = new Tache("Tache A", "Répartition du travail", 30.5);
-        String[] testDescription = {"Fin de projet","Réalisation-de-l'application", "Fin_de_projet", "    "};
+        Tache test = new Tache("Tache A", "Rï¿½partition du travail", 30.5);
+        String[] testDescription = {"Fin de projet","Rï¿½alisation-de-l'application", "Fin_de_projet", "    "};
         
-        String[] descriptionAttendus = {"Fin de projet", "Réalisation-de-l'application", "Fin_de_projet", "Répartition du travail"};
+        String[] descriptionAttendus = {"Fin de projet", "Rï¿½alisation-de-l'application", "Fin_de_projet", "Rï¿½partition du travail"};
         
         ok = true;
         for (int noTest = 0; ok && noTest < testDescription.length; noTest++) {
@@ -250,13 +258,13 @@ public class testTache {
 
     /** 
      * Test unitaires de ajouterTachePrealable
-     * @return true si test réussi, sinon false
+     * @return true si test rï¿½ussi, sinon false
      */
     private static boolean testAjouterTachePrealable() {
         boolean ok = true;
         
-        Tache tacheAAjouter = new Tache("tache à ajouter", "description de la "
-                                        + "tache à ajouter", 20.0);
+        Tache tacheAAjouter = new Tache("tache ï¿½ ajouter", "description de la "
+                                        + "tache ï¿½ ajouter", 20.0);
         Tache tacheDeTest = new Tache("tache de test", "description de test"
                                       , 10.0);
         ArrayList<Tache> tachesPrealablesAttendues = new ArrayList<Tache>();
@@ -283,17 +291,17 @@ public class testTache {
     
     /** 
      * Test unitaires de enleverTachePrealable
-     * @return true si test réussi, sinon false
+     * @return true si test rï¿½ussi, sinon false
      */
     private static boolean testEnleverTachePrealable() {
         boolean ok = true;
         
-        Tache tacheAAjouter1 = new Tache("tache à ajouter 1", "description de "
-                                         + "la tache à ajouter", 20.0);
-        Tache tacheAAjouter2 = new Tache("tache à ajouter 2", "description de "
-                                         + "la tache à ajouter", 20.0);
-        Tache tacheAAjouter3 = new Tache("tache à ajouter 3", "description de "
-                                         + "la tache à ajouter", 20.0);
+        Tache tacheAAjouter1 = new Tache("tache ï¿½ ajouter 1", "description de "
+                                         + "la tache ï¿½ ajouter", 20.0);
+        Tache tacheAAjouter2 = new Tache("tache ï¿½ ajouter 2", "description de "
+                                         + "la tache ï¿½ ajouter", 20.0);
+        Tache tacheAAjouter3 = new Tache("tache ï¿½ ajouter 3", "description de "
+                                         + "la tache ï¿½ ajouter", 20.0);
         Tache tacheDeTest = new Tache("tache de test", "description de test"
                                       , 10.0);
         tacheDeTest.ajouterTachePrealable(tacheAAjouter1);
@@ -320,29 +328,29 @@ public class testTache {
     
     /**
      * Test unitaires de toString
-     * @return true si test réussi, sinon false
+     * @return true si test rï¿½ussi, sinon false
      */
     private static boolean testToString() {
     	
-    	String attenduA = "Cette tache est défini par :\n  Nom : Tache A"
-				            + "\n  Description : Répartition du travail"
+    	String attenduA = "Cette tache est dï¿½fini par :\n  Nom : Tache A"
+				            + "\n  Description : Rï¿½partition du travail"
 				            + "\n  Duree : 29.0"
-				            + "\n  Cette tache n'a pas de taches préalables";
+				            + "\n  Cette tache n'a pas de taches prï¿½alables";
     	
-    	String attenduB =  "Cette tache est défini par :\n  Nom : Tache B"
-				            + "\n  Description : Création d'une base de donnée"
+    	String attenduB =  "Cette tache est dï¿½fini par :\n  Nom : Tache B"
+				            + "\n  Description : Crï¿½ation d'une base de donnï¿½e"
 				            + "\n  Duree : 10.0"
-				            + "\n  Taches préalables : Tache A | ";
+				            + "\n  Taches prï¿½alables : Tache A | ";
     	
-    	String attenduD =  "Cette tache est défini par :\n  Nom : Tache D"
+    	String attenduD =  "Cette tache est dï¿½fini par :\n  Nom : Tache D"
 				            + "\n  Description : Developpement"
 				            + "\n  Duree : 60.0"
-				            + "\n  Taches préalables : Tache A | Tache C | ";
+				            + "\n  Taches prï¿½alables : Tache A | Tache C | ";
     	boolean ok;
     	
-    	Tache tacheA = new Tache("Tache A", "Répartition du travail", 29);
-    	Tache tacheB = new Tache("Tache B", "Création d'une base de donnée", 10);
-    	Tache tacheC = new Tache("Tache C", "Création d'un diagramme objet", 20);
+    	Tache tacheA = new Tache("Tache A", "Rï¿½partition du travail", 29);
+    	Tache tacheB = new Tache("Tache B", "Crï¿½ation d'une base de donnï¿½e", 10);
+    	Tache tacheC = new Tache("Tache C", "Crï¿½ation d'un diagramme objet", 20);
     	Tache tacheD = new Tache("Tache D", "Developpement", 60);
     	
     	tacheB.ajouterTachePrealable(tacheA);
@@ -354,6 +362,90 @@ public class testTache {
         ok &= assertionTest.assurerEgalite(attenduB, tacheB.toString());
         ok &= assertionTest.assurerEgalite(attenduD, tacheD.toString());        
         
+        return ok;
+    }
+    
+    /**
+     * Tests unitaires de getDateAuPlusTot
+     * @return true si test validÃ© sinon false
+     */
+    private static boolean testGetDateAuPlusTot() {
+        
+        double[] dateAuPlusTotAttendus = {2.0, 7.0, 7.0, 12.0};
+        boolean ok;
+        
+        ok = true;
+        for (int noJeu = 0; ok && noJeu < aTester.length; noJeu++) {
+            ok &= assertionTest.assurerEgaliteDouble(dateAuPlusTotAttendus[noJeu],
+                                               aTester[noJeu].getDateAuPlusTot());
+        }
+        return ok;
+    }
+    
+    /**
+     * Tests unitaires de getDateAuPlusTard
+     * @return true si test validÃ© sinon false
+     */
+    private static boolean testGetDateAuPlusTard() {
+        
+        double[] dateAuPlusTotAttendus = {8.0, 12.0, 12.5, 42.0};
+        boolean ok;
+        
+        ok = true;
+        for (int noJeu = 0; ok && noJeu < aTester.length; noJeu++) {
+            ok &= assertionTest.assurerEgaliteDouble(dateAuPlusTotAttendus[noJeu],
+                                               aTester[noJeu].getDateAuPlusTard());
+        }
+        return ok;
+    }
+    
+    /** Test unitaires de setDateAuPlusTot
+     * @return true si test reussis sinon false 
+     */
+    private static boolean testSetDateAuPlusTot() {
+        
+        boolean ok;
+        
+        /** Tache de test */
+        Tache test = new Tache("Tache D", "RÃ©alisation de l'application", 30.0, 12.0, 42.0)
+        double[] testDateAuPlusTot = {78, 0.0, -5};
+        
+        double[] DateAuPlusTotAttendus = {78, 12, 30.5};
+        
+        ok = true;
+        for (int noTest = 0; ok && noTest < testDateAuPlusTot.length; noTest++) {
+            try {
+                test.setDuree(testDateAuPlusTot[noTest]);
+                ok &= test.getDateAuPlusTot() == DateAuPlusTotAttendus[noTest];
+            } catch (IllegalArgumentException DateAuPlusTotIncorrect) {
+                ok = true;
+            }
+        }
+        return ok;
+    }
+    
+    /** Test unitaires de setDateAuPlusTard
+     * @return true si test reussis sinon false 
+     */
+    private static boolean testSetDateAuPlusTard() {
+        
+        boolean ok;
+        
+        /** Tache de test */
+        Tache test = new Tache("Tache D", "RÃ©alisation de l'application", 30.0, 12.0, 42.0)
+        double[] testDateAuPlusTard = {78, 0.0, -5};
+        
+        double[] DateAuPlusTardAttendus = {78, 12, 30.5};
+        
+        ok = true;
+        for (int noTest = 0; ok && noTest < testDateAuPlusTard.length; noTest++) {
+            try {
+                test.setDuree(testDateAuPlusTard[noTest]);
+                ok &= test.getDateAuPlusTard() == DateAuPlusTardAttendus[noTest];
+            } catch (IllegalArgumentException DateAuPlusTardIncorrect) {
+                ok = true;
+            }
+        }
         return ok;
     }
 
